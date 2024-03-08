dbb0eaac4962e0b177766ce2886dd83f83cd56c9
Linqing <62589604+Lanceart@users.noreply.github.com>
2024-03-07 16:44:56 -0800

    step: bug fixed

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -15,7 +15,7 @@ const Gallery = () => {
   };
   useEffect(() => {
     // Replace 'http://example.com/api/data' with the actual URL of the webpage/API
-    fetch('https://gallery-api-o5vw.onrender.com/list-db')
+    fetch('http://image-store-api.vercel.app/list-db')
       .then(response => response.json())
       .then(data => {
         const urls = data.Items.map(item => constructImageUrl(item.key));
@@ -65,7 +65,7 @@ const Gallery = () => {
 function constructImageUrl(key) {
   // For demonstration purposes, assuming the key is the URL
   // In a real scenario, you might need to prepend a base URL or add a file extension
-  return 'https://gallery-api-o5vw.onrender.com/image/' + `${key}`;
+  return 'http://image-store-api.vercel.app/image/' + `${key}`;
 }
 
 export default Gallery;
